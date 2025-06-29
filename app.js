099977cdeeb7faa6b45bf986ad3621317094ebee
M.Meerzaman <m.meerzaman944@gmail.com>
2025-06-30 00:40:42 +0500

    DICE ROTATION FUNTIONALITY USING JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,10 @@
+const buttonEl = document.getElementById("roll-button");
+const diceEl = document.getElementById('dice');
+
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+  setTimeout(() => {
+    diceEl.classList.remove("roll-animation");
+    rollDice();
+  }, 1000);
+});
